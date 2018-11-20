b769180aee77ede8e7a28e4a2a8e3c37e5eed030
Jeswin <jeswincyriac.k@gmail.com>
2018-11-20 18:29:48 +0530

    fdf

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,HttpResponse
-
+import sqlite3
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    conn=sqlite3.connect('SQL/Main.db')
+    c=conn.cursor()
+    channel1subs=c.execute('''SELECT no_subs from youtubers where channel_name="test"''').fetchone()
+    channel2subs=c.execute('''SELECT no_subs from youtubers where channel_name="pewdiepie"''').fetchone()
+    conn.close()
+    return render(request,'home.html',{'no_subs1': channel1subs,'no_subs2':channel2subs})
